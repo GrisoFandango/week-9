2d799574154dbc8497bee05a235d20c7d90d75ec
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-12-08 18:03:26 +0000

    Update and rename 2 - Class 2 to 2 - Class 2.py
    adding comments

--- 2 - Class 2.py
+++ 2 - Class 2.py
@@ -6,11 +6,12 @@ class people():
 
     def showDetails(self):
         print(self.name,self.age,self.gender)
-
+#creation of different class objects
 p1= people("John",18,"M")
 p2= people("Mary",25,"F")
 p3= people("Kevin",22,"M")
 p4= people("Olga",32,"F")
+#using the method with each different object
 p1.showDetails()
 p2.showDetails()
 p3.showDetails()

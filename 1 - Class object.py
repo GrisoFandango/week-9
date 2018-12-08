ef420ae716b5198216e55e5590adb847ca06d957
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-12-08 17:49:38 +0000

    Update and rename 1 - Class object to 1 - Class object.py
    adding comments

--- 1 - Class object.py
+++ 1 - Class object.py
@@ -1,9 +1,11 @@
+#Creating a class
 class People():
+    #initialization of a class 
     def __init__(self, name, age, gender):
         self.name = name
         self.age = age
         self.gender = gender
-
+    #method to create a behaviour of the object
     def showDetails(self):
         print(self.name,self.age,self.gender)
 
